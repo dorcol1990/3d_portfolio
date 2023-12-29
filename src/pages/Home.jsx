1f3909fc7cca633e818672f60fd89c06892eda01
dorcol1990 <tickets1312@yahoo.com>
2023-12-29 23:43:19 +0100

    add 3d scene

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,26 @@
 import { Canvas } from "@react-three/fiber";
+import Island from "../models/island";
 import Loader from "../components/Loader";
 import { Suspense } from "react";
 
 { /*  <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
 
 </div>*/}
+
+const adjustIslandForScreenSize = ()=>{
+  let screenScale = null;
+  let screenPosition = [0, -6.5, -43];
+  let rotation = [0.1, 4.7, 0]
+
+  if(window.innerWidth < 768){
+    screenScale = [0.9, 0.9, 0.9];
+  }else{
+    screenScale=[1, 1, 1]
+  }
+  return [screenScale, screenPosition, rotation]
+}
+
+const [islandScale, islandPosition, islandRotation] = adjustIslandForScreenSize();
 
 const Home = () => {
   return (
@@ -14,11 +30,15 @@ const Home = () => {
        camera={{near: 0.1, far: 1000}}
       >
         <Suspense fallback={<Loader />}>
-          <directionalLight />
-          <ambientLight />
-          <pointLight />
-          <spotLight />
-          <hemisphereLight />
+          <directionalLight position={[1, 1, 1]} intensity={2}/>
+          <ambientLight intensity={0.5}/>
+          <hemisphereLight skyColor="#b1e1ff" groundColor="#000000" intensity={1}/>
+
+          <Island 
+           position={islandPosition}
+           scale={islandScale}
+           rotation={islandRotation}
+          />
         </Suspense>
       </Canvas>
     </section>
